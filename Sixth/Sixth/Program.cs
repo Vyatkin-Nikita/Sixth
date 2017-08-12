cf6d8b46af24ae61e58cdf1ebb4d0a61379161ab
Вяткин Никита <Vyatkin-nikita@mail.ru>
2017-08-12 22:28:30 +0500

    Готовый вариант программы. Версия 1.0

--- Sixth/Sixth/Program.cs
+++ Sixth/Sixth/Program.cs
@@ -12,51 +12,104 @@ namespace Sixth
     */
     class Program
     {
-        static int a1, a2, a3;//Первые элементы последовательности
+        static double a1, a2, a3;//Первые элементы последовательности
         static int N;//Число N
-        static int E;//Число Е
+        static double E;//Число Е
+        static int[] NumberList;//Массив, в котором хранятся номера чисел, соответствующих условию
+        static int CountNum = 0;//Cчётчик количества элементов в последовательности
         static int count = 0;//Счётчик количества элементов, такие что | ак  – ак–1 | > E
-        static int VvodProverka(int mogr = 0, int bogr = 0)//Проверка вводимых с клавиатуры чисел, mogr и bogr - минимально и максимально возможные значения числа
+        static double Check()//Проверка вводимых с клавиатуры данных (данные должны быть числами)
         {
             bool ok;
-            int n;
+            double n;//искомое число
             do
             {
-                ok = int.TryParse(Console.ReadLine(), out n);
-                if (!ok) { Console.WriteLine("Ошибка. Неверный формат данных. Повторите ввод."); continue; }
-                if ((n < mogr) && (bogr < mogr)) { Console.WriteLine("Ошибка. Число должно быть больше или равно {0} . Повторите ввод.", mogr); ok = false; continue; }
-                if ((n < mogr) && (mogr != 0) || ((n > bogr) && (bogr != 0))) { Console.WriteLine("Ошибка. Число должно находится в пределах от {0} до {1} . Повторите ввод.", mogr, bogr); ok = false; }
+                ok = double.TryParse(Console.ReadLine(), out n);
+                if (!ok) { Console.WriteLine("Ошибка. Неверный формат данных. Повторите ввод."); }
 
-            } while (!ok);
+            } while (!ok);//если введённые данные - не число, то новая итерация цикла
             Console.WriteLine();
             return n;
         }
-        static void CreateNewNumber(int a1, int a2, int a3)
+        static void CreateNewNumber(double a1, double a2, double a3)//Создание нового элемента последовательности (рекурсия)
         {
-            int ak = a3 + 2 * a2 * a1;
 
-            if (Math.Abs(ak - a3) > E) { count++; Console.Write("-"); Console.ForegroundColor = ConsoleColor.DarkGreen; Console.Write(ak); Console.ResetColor(); }
-            else Console.Write("-"+ak);
+            double ak = a3 + 2 * a2 * a1;//Новый элемент последовательности ("самый правый")
+            CountNum++;
 
-            if (count < N) CreateNewNumber(a2, a3, ak);
+            if (Math.Abs(ak - a3) > E)//Если число соответствует условиям - оно печатается зелёным, а его номер записывается в массив, если нет - то печатается белым цветом
+            {
+                NumberList[count] = CountNum; count++; Console.Write("-");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write(ak); Console.ResetColor();
+            }
+            else Console.Write("-" + ak);
+
+            if (count < N) CreateNewNumber(a2, a3, ak);//Если количество чисел, соответствующих условию меньше N, то новый этап рекурсии
         }
-        static void MainFunction()
+        static void MainFunction()//Ввод начальных данных и вызов рекурсии
         {
-            a1 = 0;
-            a2 = 0;
-            a3 = 0;
-            N = 5;
-            E = 3;
+            Console.WriteLine("Введите а1");
+            a1 = Check();
+            Console.WriteLine("Введите а2");
+            a2 = Check();
+            Console.WriteLine("Введите а3");
+            a3 = Check();
 
-            Console.Write(a1+"-");
-            if (Math.Abs(a2 - a1) > E) { count++; Console.ForegroundColor = ConsoleColor.DarkGreen; Console.Write(a2); Console.ResetColor(); }
-            else { Console.Write(a2);}
+            if (a1 == 0 && a2 == 0 && a3 == 0) { Console.WriteLine("При таких начальных числах в последовательности не будет ни одного числа, удовлетворяющего условию. Попробуйте задать другие данные."); MainFunction(); }
+
+            Console.WriteLine("Введите N");
+            //Ввод проверка N (N должно быть больше 0)
+            bool ok;
+            do
+            {
+                ok = int.TryParse(Console.ReadLine(), out N);
+                if (!ok) { Console.WriteLine("Ошибка. Неверный формат данных. Повторите ввод."); continue; }
+                if (N < 1) { Console.WriteLine("Ошибка. Число должно быть больше 0. Повторите ввод."); ok = false; }
+
+            } while (!ok);
+            NumberList = new int[N];
+
+            Console.WriteLine("Введите E");
+            //Ввод проверка Е (Е должно быть больше или равно 0)
+            do
+            {
+                ok = double.TryParse(Console.ReadLine(), out E);
+                if (!ok) { Console.WriteLine("Ошибка. Неверный формат данных. Повторите ввод."); continue; }
+                if (E < 0) { Console.WriteLine("Ошибка. Число должно быть больше или равно 0. Повторите ввод."); ok = false; }
+
+            } while (!ok);
+
+            //Проверка второго и третьего изначального числа на соответствие условию
+            CountNum = 2;
+            Console.Write(a1 + "-");
+            if (Math.Abs(a2 - a1) > E)
+            {
+                NumberList[count] = CountNum; count++;
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write(a2); Console.ResetColor();
+            }
+            else { Console.Write(a2); }
+
+            CountNum++;
             Console.Write("-");
-            if (Math.Abs(a3 - a2) > E) { count++; Console.ForegroundColor = ConsoleColor.DarkGreen; Console.Write(a3); Console.ResetColor(); }
+            if (Math.Abs(a3 - a2) > E)
+            {
+                NumberList[count] = CountNum; count++;
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write(a3); Console.ResetColor();
+            }
             else { Console.Write(a3); }
 
-            CreateNewNumber(a1, a2, a3);
-        } 
+            CreateNewNumber(a1, a2, a3);//Вывоз рекурсии
+
+            //Вывод номеров выделенных чисел
+            Console.WriteLine();
+            for (int i = 0; i < NumberList.Length; i++)
+            {
+                Console.Write(NumberList[i] + ", ");
+            }
+        }
         static void Main(string[] args)
         {
             MainFunction();
